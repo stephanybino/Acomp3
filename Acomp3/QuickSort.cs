5f1a90b9203ce596bd53e2baff83ab3a12b95009
Stephany Ghiotto Lima Bino Serri <20171ENEL0403@cefetes.br>
2019-08-30 17:57:09 -0300

    tentativa e erro

--- Acomp3/QuickSort.cs
+++ Acomp3/QuickSort.cs
@@ -6,17 +6,38 @@ namespace Acomp3
     {
         public void Part(int[] E, int left, int right)
         {
-            throw new NotImplementedException();
+            int c = E[right]; //pivô
+            int t, j = left;
+            for (int k = left; k < right; k++)
+            {
+                if (E[k] <= c)
+                {
+                    t = E[j];
+                    E[j] = E[k];
+                    E[k] = t;
+                    j++;
+                }
+           
+            }
+            t = E[j];
+            E[j] = E[right];
+            E[right] = t;
+            return j;
         }
 
         public void Sort(int[] E, int left, int right)
         {
-            throw new NotImplementedException();
+           if ( left < right)
+            {
+                int b = Part(E, left, right);
+                Sort(E, left, b-1);
+                Sort(E, b+1, right);
+            }
         }
 
         public void Sort(int[] E, int n)
         {
-            throw new NotImplementedException();
+             Sort(E, 0, n - 1); 
         }
     }
 }
--- Acomp3/TailQuickSort.cs
+++ Acomp3/TailQuickSort.cs
@@ -6,17 +6,45 @@ namespace Acomp3
     {
         public void Part(int[] E, int left, int right)
         {
-            throw new NotImplementedException();
+            int c = E[right]; //pivô
+            int t, j = left;
+            for (int k = left; k < right; k++)
+            {
+                if (E[k] <= c)
+                {
+                    t = E[j];
+                    E[j] = E[k];
+                    E[k] = t;
+                    j++;
+                }
+              
+            }
+            t = E[j];
+            E[j] = E[right];
+            E[right] = t;
+            return j;
         }
-
         public void Sort(int[] E, int left, int right)
         {
-            throw new NotImplementedException();
+           while(left < right)
+                {
+                    int s = Part(E, left, right);
+                    if ((s - left) < right - s)
+                    {
+                        Sort(E, left, s - 1);
+                        left = s + 1;
+                    }
+                    else
+                    {
+                        Sort(E, s + 1, right);
+                        right = s - 1;
+                    }
+                }
         }
 
         public void Sort(int[] E, int n)
         {
-            throw new NotImplementedException();
+            Sort(E, 0, n - 1);
         }
     }
 }
